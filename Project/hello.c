6f42e24f11907d87be7fe0abaf954d045275c5ca
wujingwu <wujingwu_shaoy@foxmail.com>
2020-12-02 21:00:12 +0800

    OK

--- Project/hello.c
+++ Project/hello.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
 void main() {
+    int n1 = 9;
+    int n2 = 8;
+    int n3 = 7;
+    
+    int temp = 0;
 
-      char name[100] = "";
-      printf("you age \n");
-      scanf("%s",&name);
-      printf("you age is %s\n",name);
-      getchar();
-      getchar();	
+    if(n1 > n2){
+          temp = n2;
+          n2 = n1;
+          n1 = temp;
+    }
+
+     printf("第一次交换的顺序%d%d%d\n",n1,n2,n3);
+
+      if(n2 > n3){
+            temp = n3;
+            n3 = n2;
+            n2 = temp;
+      }
+
+      printf("第二次交换的顺序%d%d%d\n",n1,n2,n3);
+
+
+       if(n1 > n2){
+          temp = n2;
+          n2 = n1;
+          n1 = temp;
+       }
+
+      printf("第三次交换的顺序%d%d%d\n",n1,n2,n3);
+
     
 }
