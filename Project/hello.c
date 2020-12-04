d84ab1dfd59e14a5421a194db41446b6243246af
wujingwu <wujingwu_shaoy@foxmail.com>
2020-12-04 18:01:57 +0800

    2223

--- Project/hello.c
+++ Project/hello.c
@@ -1,36 +1,19 @@
 #include <stdio.h>
 
 void main() {
-    int n1 = 9;
-    int n2 = 8;
-    int n3 = 7;
-    
-    int temp = 0;
-
-    if(n1 > n2){
-          temp = n2;
-          n2 = n1;
-          n1 = temp;
-    }
-
-     printf("第一次交换的顺序%d%d%d\n",n1,n2,n3);
-
-      if(n2 > n3){
-            temp = n3;
-            n3 = n2;
-            n2 = temp;
+     
+      double a = 0.0;
+      printf("plase score \n");
+      scanf("%lf",&a);
+      if(a  > 90){
+        printf("\nget BMW");
+      }else if (a > 80)      {
+        printf("\nget iphone");
+      }else if (a > 60)      {
+        printf("\nget wanju");
+      }else {
+        printf("\n no jiangli");
       }
-
-      printf("第二次交换的顺序%d%d%d\n",n1,n2,n3);
-
-
-       if(n1 > n2){
-          temp = n2;
-          n2 = n1;
-          n1 = temp;
-       }
-
-      printf("第三次交换的顺序%d%d%d\n",n1,n2,n3);
-
-    
+    getchar();
+    getchar();
 }
