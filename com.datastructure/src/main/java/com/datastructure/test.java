284a144fb79dbd0616d8e0dd51f871881e60dcb6
wujingwu <wujingwu_shaoy@foxmail.com>
2021-01-22 17:55:46 +0800

    数组模拟环形队列

--- com.datastructure/src/main/java/com/datastructure/test.java
+++ com.datastructure/src/main/java/com/datastructure/test.java
@@ -1,66 +1,137 @@
 package com.datastructure;
 
+import javax.management.RuntimeErrorException;
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        spareArray();
-    }
+        //spareArray();
+        ArrayQueue arrayQueue = new ArrayQueue(5);
+        char key = ' ';//接收用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("g(get):从队列取出数据");
+            System.out.println("h(head):查看队列头数据");
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输入一个数");
+                    try {
+                        int n = scanner.nextInt();
+                        arrayQueue.addQueue(n);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'g':
+                    try {
+                        int q = arrayQueue.removeQueue();
+                        System.out.printf("取出来的的数据是%d\n", q);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
 
-    static void spareArray() {
-        int[][] arr = new int[11][11];
-        arr[1][2] = 1;
-        arr[2][3] = 2;
-        System.out.println("原始二维数组");
-
-        //遍历数组，得到非0的数据的个数
-        int sum = 0;
-        for (int[] row : arr) {
-            for (int data : row) {
-                System.out.printf("%d\t", data);
-                if (data != 0) {
-                    sum++;
-                }
+                    break;
+                case 'h':
+                    try {
+                        int q = arrayQueue.headQueue();
+                        System.out.printf("取出来的的数据是%d\n", q);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    System.exit(0);
+                    break;
             }
-            System.out.println();
         }
-
-        //创建稀疏数组
-        int[][] sparseArray = new int[sum + 1][sum + 1];
-        sparseArray[0][0] = arr.length;//二维数组的行
-        sparseArray[0][1] = arr[0].length;//二维数组的列
-        sparseArray[0][2] = sum;           //二维数组不为0的个数
-        //记录不为0的数值的所在的位置
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] != 0) {
-                    count++;
-                    sparseArray[count][0] = i;
-                    sparseArray[count][1] = j;
-                    sparseArray[count][2] = arr[i][j];
-                }
-            }
-        }
-        System.out.println();
-        System.out.println("二维数组转换后的稀疏数组");
-        for (int[] row : sparseArray) {
-            for (int data : row) {
-                System.out.printf("%d\t", data);
-            }
-            System.out.println();
-        }
-
-        //将稀疏数组在转原始数组
-        int[][] arr2 = new int[sparseArray[0][0]][sparseArray[0][1]];
-        for (int i = 1; i < sparseArray.length; i++) {
-            arr2[sparseArray[i][0]][sparseArray[i][1]] = sparseArray[i][2];
-        }
-
-        System.out.println("稀疏数组转换为二维数组");
-        for (int[] row : arr2) {
-            for (int data : row) {
-                System.out.printf("%d\t", data);
-            }
-            System.out.println();
-        }
+        System.out.println("程序退出");
     }
 }
+
+class ArrayQueue {
+
+    //数组模拟队列
+    int arrayMax;//队列最大值
+    int front;//队列头
+    int rear;//队列尾
+    int[] arr;//模拟队列的数组
+
+    public ArrayQueue(int arrayMaxSize) {
+        arrayMax = arrayMaxSize;
+        arr = new int[arrayMaxSize];
+        front = -1;
+        rear = -1;
+    }
+
+    /**
+     * 判断队列是否满了
+     */
+    public boolean isFull() {
+        return front == arrayMax - 1;
+    }
+
+    /**
+     * 判断队列是否空
+     */
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    /**
+     * 队列取数
+     */
+    public int removeQueue() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        front++;
+        return arr[front];
+    }
+
+    /**
+     * 添加数进入队列
+     *
+     * @param n
+     */
+    public void addQueue(int n) {
+        if (isFull()) {
+            System.out.println("队列满了");
+            return;
+        }
+        rear++;
+        arr[rear] = n;
+    }
+
+    /**
+     * 显示队列
+     */
+    public void showQueue() {
+        if (isEmpty()) {
+            System.out.println("队列空,无数据");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d] = %d", i, arr[i]);
+        }
+    }
+
+    /**
+     * 队列头数据
+     */
+    public int headQueue() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        return arr[front + 1];
+    }
+}
+
+
--- 笔记/java数据结构和算法.md
+++ 笔记/java数据结构和算法.md
@@ -73,7 +73,149 @@ public class tests {
 ```  
 
 ## 数组模拟队列
+1.银行排队系统
+2.队列是一个有序列表,遵循先进先出
+![数组模拟队列](数组模拟队列.jpg)
+### 数组模拟队列案例
+```java
+package com.datastructure;
 
+import javax.management.RuntimeErrorException;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        //spareArray();
+        ArrayQueue arrayQueue = new ArrayQueue(5);
+        char key = ' ';//接收用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("g(get):从队列取出数据");
+            System.out.println("h(head):查看队列头数据");
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输入一个数");
+                    try {
+                        int n = scanner.nextInt();
+                        arrayQueue.addQueue(n);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'g':
+                    try {
+                        int q = arrayQueue.removeQueue();
+                        System.out.printf("取出来的的数据是%d\n", q);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    break;
+                case 'h':
+                    try {
+                        int q = arrayQueue.headQueue();
+                        System.out.printf("取出来的的数据是%d\n", q);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    System.exit(0);
+                    break;
+            }
+        }
+        System.out.println("程序退出");
+    }
+}
+
+class ArrayQueue {
+
+    //数组模拟队列
+    int arrayMax;//队列最大值
+    int front;//队列头
+    int rear;//队列尾
+    int[] arr;//模拟队列的数组
+
+    public ArrayQueue(int arrayMaxSize) {
+        arrayMax = arrayMaxSize;
+        arr = new int[arrayMaxSize];
+        front = -1;
+        rear = -1;
+    }
+
+    /**
+     * 判断队列是否满了
+     */
+    public boolean isFull() {
+        return front == arrayMax - 1;
+    }
+
+    /**
+     * 判断队列是否空
+     */
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    /**
+     * 队列取数
+     */
+    public int removeQueue() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        front++;
+        return arr[front];
+    }
+
+    /**
+     * 添加数进入队列
+     *
+     * @param n
+     */
+    public void addQueue(int n) {
+        if (isFull()) {
+            System.out.println("队列满了");
+            return;
+        }
+        rear++;
+        arr[rear] = n;
+    }
+
+    /**
+     * 显示队列
+     */
+    public void showQueue() {
+        if (isEmpty()) {
+            System.out.println("队列空,无数据");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d] = %d", i, arr[i]);
+        }
+    }
+
+    /**
+     * 队列头数据
+     */
+    public int headQueue() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        return arr[front + 1];
+    }
+}
+
+```
+### 问题 数组不能复用，使用一次就不能使用了，没有达到循环使用的作用
 
 
 
